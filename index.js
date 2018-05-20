b509b995ebadee94d68f9c37b3f78e90a2164588
TYOHUONEPC\Ari <ari.iska.lahti@gmail.com>
2018-05-20 20:43:05 +0300

    herokuTest

--- index.js
+++ index.js
@@ -158,8 +158,7 @@ app.post('/api/persons', (request, response) => {
 
 })
 
-
-
-const port = 3001
-app.listen(port)
-console.log(`Server running on port ${port}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
